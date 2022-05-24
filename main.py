7b2338ca90c9900bf1eb7836f963844216b426ba
Alexander Peerdeman <mail@alexanderpeerdeman.de>
2022-05-24 15:25:48 +0200

    Remove webcolors dependency

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import types
 import awoxmeshlight
 import bluepy
 import paho.mqtt.client as mqtt
-import webcolors
 from awoxmeshlight import packetutils as pckt
 
 MESH_NAME = "FDCqrGLE"
@@ -15,7 +14,6 @@ MESH_PASSWORD = "3588b7f4"
 
 # receive message from mqtt -> apply to light // todo: find out if we can apply color_mode and brightness simultaneously
 # receive notification from mesh -> apply to local state of light -> publish state
-
 
 def publish_discovery_message(light):
     mesh_id = light["mesh_id"]
@@ -165,26 +163,6 @@ def on_message(client: mqtt.Client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
     match_message(msg)
-
-
-def closest_color(requested_color):
-    min_colors = {}
-    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_color[0]) ** 2
-        gd = (g_c - requested_color[1]) ** 2
-        bd = (b_c - requested_color[2]) ** 2
-        min_colors[(rd + gd + bd)] = name
-    return min_colors[min(min_colors.keys())]
-
-
-def get_color_name(requested_color):
-    try:
-        closest_name = actual_name = webcolors.rgb_to_name(requested_color)
-    except ValueError:
-        closest_name = closest_color(requested_color)
-        actual_name = None
-    return actual_name, closest_name
 
 
 def myHandleNotification(self, cHandle, data):
